75b9114cc9408c9653d9fa66c81cbdc63e9683a4
Souljeee <drobitek76106055@gmail.com>
2024-01-27 15:58:04 +0300

    feat(application): add UsersTable

---
+++ src/main/kotlin/com/cofion/database/tables/UsersTable.kt
@@ -0,0 +1,14 @@
+package com.cofion.database.tables
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.date
+
+object UsersTable: Table("users") {
+    val id = varchar("id", 100)
+    val firstName = varchar("first_name", 50).nullable()
+    val lastName = varchar("last_name", 50).nullable()
+    val login = varchar("login", 50)
+    val password = varchar("password", 100)
+    val createAccountDate = date("create_account_dt")
+    val token = varchar("token", 100).nullable()
+}
